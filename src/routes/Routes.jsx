51c6e167b86997ad225f421706c84f15fce8eddd
ahnaf4d <ahnaftahmid802@gmail.com>
2024-05-21 14:55:55 +0600

    previouse code added

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layout/Main';
+import Home from '../pages/Home/Home/Home';
+import Menu from '../pages/Menu/Menu/Menu';
+import Order from '../pages/Order/Order/Order';
+
+export const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+      {
+        path: '/menu',
+        element: <Menu></Menu>,
+      },
+      {
+        path: '/order-food',
+        element: <Order></Order>,
+      },
+    ],
+  },
+]);
